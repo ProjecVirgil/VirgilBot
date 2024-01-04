89e9e93f2d4706bd9a1df9bb7898ac7b6f3eded3
Retr0 <dragonfaiver@gmail.com>
2024-01-04 14:49:14 +0100

    feat: Add VirgilBot class and load_cogs method
    - Adds the VirgilBot class which represents the main bot class for the Virgil Discord Bot.
    - Implements the load_cogs method which loads all cogs in the 'cogs' directory and adds them to the bot.

--- src/main.py
+++ src/main.py
@@ -1,12 +1,55 @@
-import os
+import platform
 
-from dotenv import load_dotenv
+import nextcord
+from nextcord.ext import commands
+from pyfiglet import figlet_format
 
-from src.logger import logging
+from config import DISCORD_API_TOKEN
+from logger import logging
 
-load_dotenv()
+TESTING_GUILD_ID = 1192420850993999923
 
+class VirgilBot(commands.Bot):
+    """The main bot class for the Virgil Discord Bot.
 
-DISCORD_API_TOKEN = os.getenv("DISCORD_API_TOKEN")
+    Args:
+        commands (_type_): _description_
+    """
+    def __init__(self):
+        """Initializes a new instance of the `VirgilBot` class."""
+        super().__init__(command_prefix="!", intents=nextcord.Intents.all())
 
+        self.ascii_art = figlet_format("Retr0", font="slant")
+        self.cogs_list = ['shut']
+        self.load_commands()
+        self.load_cogs()
+        self.synched = ""
 
+    def load_cogs(self):
+        """Load all cogs in the 'cogs' directory and add them to the bot."""
+        for ext in self.cogs_list:
+            try:
+                self.load_extension(f'cogs.{ext}')
+                logging.info(f"Cog {ext} loaded successfully")
+            except Exception as e:
+                logging.error(f"Error in the load of cog {ext}: {e}")
+
+    async def on_ready(self):
+        """Event handler that is called when the bot connects to Discord."""
+        await self.change_presence(activity=nextcord.Game(name="writing poems"))
+        print(self.ascii_art)
+        logging.info("THE BOT IS ONLINE")
+        logging.info(f"Logged in as {self.user.name}")
+        logging.info(f"Bot ID {self.user.id}")
+        logging.info(f"nextcord Version {nextcord.__version__}")
+        logging.info(f"Python Version {platform.python_version()}")
+
+    def load_commands(self):
+        """Load all command functions from the 'bot/commands' module."""
+        @self.slash_command(name="hello",description="My first slash command", guild_ids=[TESTING_GUILD_ID])
+        async def hello(interaction: nextcord.Interaction):
+            await interaction.response.send_message("Hello!")
+
+if __name__ == '__main__':
+    bot = VirgilBot()
+    bot.run(DISCORD_API_TOKEN)
